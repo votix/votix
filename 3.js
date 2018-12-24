14fd7ee437a4d7d712ed4150e938d543457dbfe8
Votix <46107032+Votix@users.noreply.github.com>
2018-12-25 00:59:53 +0700

    Add files via upload

---
+++ 3.js
@@ -0,0 +1,22 @@
+﻿function doClrInput() {
+	document.getElementById('txtInput').value = "";
+	document.getElementById("txtInput").focus();
+}
+
+function HTMLEncode() {
+	var i = G('txtInput').value,
+		db = G('chkDouble').checked;
+	G('txtInput').value = Encoder.htmlEncode(i, db);
+}
+
+function HTMLDecode() {
+	var i = G('txtInput').value;
+	G('txtInput').value = Encoder.htmlDecode(i);
+}
+(function() {
+	var en = '';
+	G('txtInput').value = unescape(en);
+	var db = G('chkDouble').checked;
+	G('txtEncoded').value = Encoder.htmlEncode(unescape(en), db);
+	G('txtEncoded').value = Encoder.htmlDecode(G('txtEncoded').value);
+})
