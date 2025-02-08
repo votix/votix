680c53667446ab6eca32b9a6d96df37e86a0e399
Votix.ru <46107032+votix@users.noreply.github.com>
2025-02-08 13:11:55 +0700

    Create starrating33.js

---
+++ starrating33.js
@@ -0,0 +1,8 @@
+console.log("starrating30.js is running!");
+
+document.addEventListener('DOMContentLoaded', function() {
+    console.log("DOMContentLoaded handler in starrating30.js is executing!");
+    console.log("This is inside DOMContentLoaded!");
+});
+
+console.log("starrating30.js finished parsing.");
