53f141e52398fc6bb2e4cb3d7807b9172f41f4af
Votix.ru <46107032+votix@users.noreply.github.com>
2025-02-08 14:34:25 +0700

    Delete starrating33.js

--- starrating33.js
+++
@@ -1,8 +0,0 @@
-console.log("starrating30.js is running!");
-
-document.addEventListener('DOMContentLoaded', function() {
-    console.log("DOMContentLoaded handler in starrating30.js is executing!");
-    console.log("This is inside DOMContentLoaded!");
-});
-
-console.log("starrating30.js finished parsing.");
